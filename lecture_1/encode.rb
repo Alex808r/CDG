a0df1e67ccd05472b3cc0b260c3979eb9174380a
Alex808r <fromxlam@gmail.com>
2021-07-07 13:17:10 +0300

    add encode info to Windows

---
+++ lecture_1/encode.rb
@@ -0,0 +1,11 @@
+# XXX/ Этот код необходим только при использовании русских букв на Windows
+# Ультимативное решение -- вставить в начале файла код
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+# /XXX
