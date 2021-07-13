6d40c3ccc489e30f608d2b90497ed02468e5d785
Alex808r <fromxlam@gmail.com>
2021-07-13 17:12:51 +0300

    refactor PostController

--- lecture_2/Task_4/router.rb
+++ lecture_2/Task_4/router.rb
@@ -1,27 +1,27 @@
-odule Resource
-def connection(routes)
-  if routes.nil?
-    puts "No route matches for #{self}"
-    return
-  end
-
-  loop do
-    print 'Choose verb to interact with resources (GET/POST/PUT/DELETE) / q to exit: '
-    verb = gets.chomp.upcase
-    break if verb == 'q'
-
-    action = nil
-
-    if verb == 'GET'
-      print 'Choose action (index/show) / q to exit: '
-      action = gets.chomp
-      break if action == 'q'
+module Resource
+  def connection(routes)
+    if routes.nil?
+      puts "No route matches for #{self}"
+      return
     end
 
+    loop do
+      print 'Choose verb to interact with resources (GET/POST/PUT/DELETE) / q to exit: '
+      verb = gets.chomp.upcase
+      break if verb == 'q'
 
-    action.nil? ? routes[verb].call : routes[verb][action].call
+      action = nil
+
+      if verb == 'GET'
+        print 'Choose action (index/show) / q to exit: '
+        action = gets.chomp
+        break if action == 'q'
+      end
+
+
+      action.nil? ? routes[verb].call : routes[verb][action].call
+    end
   end
-end
 end
 
 class PostsController
